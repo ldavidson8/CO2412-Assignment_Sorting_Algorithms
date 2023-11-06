42c45236c7e0175a4bf78e8f94a8df56e96f3f93
lewisdavidson <lewisdavidson8@gmail.com>
2023-11-06 17:00:08 +0000

    Refactor sort functions and arrays into their own arrays. Iterates through them instead

--- rng_data_sorter.py
+++ rng_data_sorter.py
@@ -8,26 +8,23 @@ from quick_sort import quick_sort as qsort
 NUMBER_MIN = 100000
 NUMBER_MAX = 999999
 
-ARRAY1_SIZE = 100
-ARRAY2_SIZE = 1000
-ARRAY3_SIZE = 10000
 
-array1 = [random.randint(NUMBER_MIN, NUMBER_MAX) for _ in range(ARRAY1_SIZE)]
-array2 = [random.randint(NUMBER_MIN, NUMBER_MAX) for _ in range(ARRAY2_SIZE)]
-array3 = [random.randint(NUMBER_MIN, NUMBER_MAX) for _ in range(ARRAY3_SIZE)]
+# If you wish to create another array, add another whole number inside this array
+ARRAY_SIZES = [100, 1000, 10000]
+
+sorting_functions = [ssort, msort, qsort]
+
+arrays = []
+for size in ARRAY_SIZES:
+    array = [random.randint(NUMBER_MIN, NUMBER_MAX) for _ in range(size)]
+    arrays.append(array)
 
 # Create copies of the arrays for sorting
-copy_array1 = copy.deepcopy(array1)
-copy_array2 = copy.deepcopy(array2)
-copy_array3 = copy.deepcopy(array3)
+copy_arrays = [copy.deepcopy(array) for array in arrays]
 
 # Run each sorting algorithm five times and record execution time
-for array, array_size in [(copy_array1, ARRAY1_SIZE), (copy_array2, ARRAY2_SIZE), (copy_array3, ARRAY3_SIZE)]:
-    time_taken = timeit.timeit(lambda: msort(array), number=5)
-    print(f"merge_sort on array size {array_size}: {time_taken:.6f} seconds")
-
-    time_taken = timeit.timeit(lambda: ssort(array), number=5)
-    print(f"selection_sort on array size {array_size}: {time_taken:.6f} seconds")
-
-    time_taken = timeit.timeit(lambda: qsort(array), number=5)
-    print(f"quick_sort on array size {array_size}: {time_taken:.6f} seconds")
+for sort_func in sorting_functions:
+    print(f"Sorting with {sort_func.__name__}")
+    for array, array_size in zip(copy_arrays, ARRAY_SIZES):
+        time_taken = timeit.timeit(lambda: sort_func(array), number=5)
+        print(f"Array size {array_size}: {time_taken:.6f} seconds")
