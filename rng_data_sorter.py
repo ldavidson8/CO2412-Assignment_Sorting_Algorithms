b35a20a5a7cc0553ac6212fc90c942c9065bcbc8
lewisdavidson <lewisdavidson8@gmail.com>
2023-11-16 00:26:42 +0000

    Delete old debug block

--- rng_data_sorter.py
+++ rng_data_sorter.py
@@ -31,17 +31,13 @@ run_times_dict = {sort_func.__name__: [] for sort_func in sorting_functions}
 for sort_func in sorting_functions:
     print(f"Sorting with {sort_func.__name__}")
     for array, array_size in zip(copy_arrays, ARRAY_SIZES):
-        # Record the times here, for debugging uncomment the code block below
-
-        # time_taken = timeit.timeit(lambda: sort_func(array), number=5)
-        # print(f"Array size {array_size}: {time_taken:.6f} seconds")
-
-        
         run_times = []
+
         for _ in range(5):  # Run each function 5 times
             run_time = timeit.timeit(lambda: sort_func(array), number=1)
             run_times.append(run_time)
             print(f"Array size {array_size}, Run {len(run_times)}: {run_time:.6f} seconds")
+            
         run_times_dict[sort_func.__name__].append(run_times)
 
 for sort_func in sorting_functions:
@@ -49,6 +45,7 @@ for sort_func in sorting_functions:
 
 plt.xlabel('Data Size')
 plt.ylabel('Execution Time (seconds)')
+plt.yscale('log')
 plt.title('Algorithm Performance Comparison')
 plt.legend()
 plt.show()
