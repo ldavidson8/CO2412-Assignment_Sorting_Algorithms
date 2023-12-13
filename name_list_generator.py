4eaa390d55a16b75beb2df153f5a018a74ec50d1
Lewis Davidson <lewisdavidson8@gmail.com>
2023-12-13 11:32:49 +0000

    Rename variable, remove unneccesary variable assignment

--- name_list_generator.py
+++ name_list_generator.py
@@ -1,20 +1,22 @@
 import random
 
-MAX_LINES = 4000
+NUM_OF_NAMES = 4000
 
 
 # Read and trim a file to 4,000 lines
-def read_and_trim_file(filename, max_lines=MAX_LINES):
+def read_and_trim_file(filename):
     with open(filename, "r") as file:
-        lines = file.read().splitlines()[:max_lines]
+        lines = file.read().splitlines()[:NUM_OF_NAMES]
     return lines
+
 
 first_names = read_and_trim_file("firstNames.txt")
 last_names = read_and_trim_file("lastNames.txt")
 
 # Generate a list of full names by combining random first and last names
 full_names = [
-    random.choice(first_names) + " " + random.choice(last_names) for _ in range(4000)
+    random.choice(first_names) + " " + random.choice(last_names)
+    for _ in range(NUM_OF_NAMES)
 ]
 
 with open("fullNames.txt", "w") as full_names_f:
